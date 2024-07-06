0e111050fff8bf3492fdfe4d3479371f1c56137d
Methush Anjula <methushanjula01@gmail.com>
2024-07-06 19:15:50 +0530

    learning components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import PartOne from './components/PartOne';
+import Home from './pages/home'
 
 function App() {
   return (
     <div className="App">
-      <h1>This is my first react project</h1>
-      <PartOne/>
+        <Home/>
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopicBox from '../components/TopicBox'
+import Content from '../components/Content'
+
+export default function home() {
+  return (
+    <div>
+        <TopicBox/>
+        <Content/>
+    </div>
+  )
+}
