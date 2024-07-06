99553eb38d99a059c1eb7d1213cb3ab70169e32c
Methush Anjula <methushanjula01@gmail.com>
2024-07-06 19:41:24 +0530

    learning dynamic components

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -4,6 +4,7 @@ import './topicbox.css'
 export default function TopicBox() {
     const topic = "My favourite game is valorant"
     const item = " and GOT"
+    const foodprices = {kottu: "250", rice: "350"} //we cannot use object and boolean as dynamic compponents
 
     const a = 10;
     const b = 20;
