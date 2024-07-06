ccd5e15f855cd9af70f375c134b08adc6997bee0
Methush Anjula <methushanjula01@gmail.com>
2024-07-06 21:27:38 +0530

    Props Handling

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import './topicbox.css'
 
-export default function TopicBox() {
-    const topic = "My favourite game is valorant"
-    const item = " and GOT"
-    const foodprices = {kottu: "250", rice: "350"} //we cannot use object and boolean as dynamic compponents
-
-    const a = 10;
-    const b = 20;
-    const c = a+b;
+export default function TopicBox(props) {
+        
   return (
     <div>
       <div className="topicBox">
-        <span className='text'>{topic} {item} {c}</span>
+        <span className='text'>My favourite game is {props.topic} and 
+            price is {props.price}</span>
+            {props.children}
       </div>
     </div>
   )
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,8 +5,16 @@ import Content from '../components/Content'
 export default function home() {
   return (
     <div>
-        <TopicBox/>
-        <Content/>
+        <TopicBox topic = "valorant" price ="200">
+          <span>This is description</span>
+        </TopicBox>
+        <TopicBox topic = "got" price ="300">
+          <button>This is a Button</button>
+        </TopicBox>
+        <TopicBox topic = "rdr2" price ="400">
+          <p>hello i am methush</p>
+        </TopicBox>
+        
     </div>
   )
 }
