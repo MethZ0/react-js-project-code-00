41a879cdb986f1123b71377b1f482e4104cb5bf8
Methush Anjula <methushanjula01@gmail.com>
2024-07-06 19:36:24 +0530

    learning dynamic components

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -2,10 +2,16 @@ import React from 'react'
 import './topicbox.css'
 
 export default function TopicBox() {
+    const topic = "My favourite game is valorant"
+    const item = " and GOT"
+
+    const a = 10;
+    const b = 20;
+    const c = a+b;
   return (
     <div>
       <div className="topicBox">
-        <span className='text'>This is the topic</span>
+        <span className='text'>{topic} {item} {c}</span>
       </div>
     </div>
   )
